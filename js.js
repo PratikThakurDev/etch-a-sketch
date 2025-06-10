7428787be41922c2421c64c4cb7b3040e6ed1883
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-06-10 21:26:26 +0530

    Completed the Project

--- js.js
+++ js.js
@@ -10,6 +10,7 @@ for (let i = 0 ; i<17 ; i++) {
     }
     content.append(div1) ;
 }
+changeBg()
 
 function random(number) {
     return Math.floor(Math.random()*number) ;
@@ -20,8 +21,49 @@ function bgChange() {
     return randomColor ;
 }
 
-const box = document.querySelectorAll('.box') ;
-box.forEach(element => {
-    element.addEventListener("mouseenter",(event) => {
-    event.target.style.backgroundColor = bgChange();
-})});
+function editGridNumber(gridNumber) {
+    removeAllDivs()
+    const content1 = document.querySelector("#container") ;
+    for (let i = 0 ; i<gridNumber ; i++) {
+        const div3 = document.createElement("div") ;
+        div3.classList.add('box1') ;
+    
+
+        for (let j =0 ; j<gridNumber ; j++){
+            const div4 = document.createElement("div") ;
+            div4.classList.add('box') ;
+            div4.style.height = '(800/gridNumber)px'
+            div3.append(div4) ;
+        }
+    
+    content1.append(div3) ;
+    }
+    changeBg()
+}
+
+function changeBg (){
+    const box = document.querySelectorAll('.box') ;
+    box.forEach(element => {
+        element.addEventListener("mouseenter",(event) => {
+        event.target.style.backgroundColor = bgChange();
+    })});
+}
+
+function removeAllDivs() {
+    const parentDiv = document.querySelector('#container') ;
+    parentDiv.innerHTML ='';
+}
+
+
+const button = document.querySelector('#button')
+button.addEventListener("click",() => {
+    let gridNumber=parseInt(prompt("Enter number of squares per sides : (less than 100)"))
+    if (gridNumber < 100) {
+        editGridNumber(gridNumber)
+    }
+    else {
+        console.log("You entered too large a number!!")
+    }
+}) ;
+
+
